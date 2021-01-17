97cb2de4a883e75ce901b4b228784a3abd1920c7
LibertyReman <65054805+LibertyReman@users.noreply.github.com>
2021-01-17 15:51:41 +0900

    props-3

--- src/components/ColorfulMessage.jsx
+++ src/components/ColorfulMessage.jsx
@@ -1,13 +1,15 @@
 import React from "react";
 
 const ColorfulMessage = (props) => {
+  const { color, children } = props;
   console.log(props);
   const contentStyle = {
-    color: props.color,
+    //color: color,
+    color,
     fontSize: "25px"
   };
 
-  return <p style={contentStyle}>{props.children}</p>;
+  return <p style={contentStyle}>{children}</p>;
 };
 
 export default ColorfulMessage;
