5e6957536232db041cbe69d67097ec6d357f1851
LibertyReman <65054805+LibertyReman@users.noreply.github.com>
2021-01-18 22:26:12 +0900

    useEffect-1

--- src/App.jsx
+++ src/App.jsx
@@ -2,19 +2,29 @@ import React, { useState } from "react";
 import ColorfulMessage from "./components/ColorfulMessage";
 
 const App = () => {
+  console.log("first");
+  //動的に変わるnum, numを更新するsetNumという関数を定義, 初期値0
+  const [num, setNum] = useState(0);
+  const [faceShowFlag, setFaceShowFlag] = useState(true);
   const onClickCountUp = () => {
     setNum(num + 1);
   };
 
-  //動的に変わるnum, numを更新するsetNumという関数を定義, 初期値0
-  const [num, setNum] = useState(0);
+  const onClickSwitchShowFlag = () => {
+    setFaceShowFlag(!faceShowFlag);
+  };
+
   return (
     <>
       <h1 style={{ color: "red" }}>こんにちは</h1>
       <ColorfulMessage color="blue">お元気ですか？</ColorfulMessage>
       <ColorfulMessage color="pink">元気です</ColorfulMessage>
       <button onClick={onClickCountUp}>カウントアップ</button>
+      <br />
+      <button onClick={onClickSwitchShowFlag}>on/off</button>
       <p>{num}</p>
+      {/*  faceShowFlagがtrueのとき右の要素を表示 */}
+      {faceShowFlag && <p>＼(^o^)／</p>}
     </>
   );
 };
--- src/components/ColorfulMessage.jsx
+++ src/components/ColorfulMessage.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 
 const ColorfulMessage = (props) => {
+  console.log("カラフル");
   const { color, children } = props;
   console.log(props);
   const contentStyle = {
